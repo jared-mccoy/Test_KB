293d34faf19e96041a195ecb67bccc57fc5cbd84
jMcCoy <jmccoy909@gmail.com>
2024-10-03 13:30:18 -0700

    Quartz sync: Oct 3, 2024, 1:30 PM

--- quartz/plugins/emitters/componentResources.ts
+++ quartz/plugins/emitters/componentResources.ts
@@ -61,7 +61,7 @@ async function joinScripts(scripts: string[]): Promise<string> {
   const script = scripts.map((script) => `(function () {${script}})();`).join("\n")
 
   const res = await transpile(script, {
-    minify: true,
+    minify: false,
     sourcemap: 'inline',  // Add this option to generate source maps
   })
 
