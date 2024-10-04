ad03fed359e387c33642fe7e366c8c38e0655ba1
jMcCoy <jmccoy909@gmail.com>
2024-10-03 19:35:32 -0700

    Adding external scripts

---
+++ content/homepage/content.js
@@ -0,0 +1,218 @@
+export const content = {
+  meta: {
+    charset: "utf-8",
+    httpEquiv: "X-UA-Compatible",
+    viewport: "width=device-width, initial-scale=1.0",
+    description: "Free HTML5 Template by FreeHTML5.co",
+    keywords: "free html5, free template, free bootstrap, html5, css3, mobile first, responsive",
+    author: "FreeHTML5.co",
+    title: "Black &mdash; Free Fully Responsive HTML5 Template by FREEHTML5.co",
+    og: {
+      title: "",
+      image: "",
+      url: "",
+      siteName: "",
+      description: "",
+    },
+    twitter: {
+      title: "",
+      image: "",
+      url: "",
+      card: "",
+    },
+  },
+  links: {
+    favicon: "favicon.ico",
+    googleFont: "https://fonts.googleapis.com/css2?family=Overpass:wght@400;600;700&display=swap",
+    animateCSS: "css/animate.css",
+    icomoonCSS: "css/icomoon.css",
+    lineIconsCSS: "css/simple-line-icons.css",
+    bootstrapCSS: "css/bootstrap.css",
+    styleCSS: "css/style.css",
+  },
+  scripts: {
+  /*   modernizr: "js/modernizr-2.6.2.min.js", */
+    jquery: "js/jquery.min.js",
+    jqueryEasing: "js/jquery.easing.1.3.js",
+    bootstrapJS: "js/bootstrap.min.js",
+    waypoints: "js/jquery.waypoints.min.js",
+    stellar: "js/jquery.stellar.min.js",
+    countTo: "js/jquery.countTo.js",
+    mainJS: "js/main.js",
+  },
+  header: {
+    home: "Home",
+    services: "Services",
+    project: "Project",
+    pricing: "Pricing",
+    team: "Team",
+  },
+  sections: {
+    home: {
+      id: "fh5co-home",
+      image: "images/Surface_Scholar_Text_Desktop.png",
+      title: "Surface / Scholar",
+      //description: `Toward a knowledge without depth`,
+    },
+    services: {
+      //title: "Mnemotext",
+      //description: 
+      title: "Knowledge Without Depth",
+      //"Writing is the texture of memory. Technology, its unraveling.",
+      gridImages: ["images/Obsidian_Screenshot.png"],
+      counters: [
+        { count: 500, label: "Authors" },
+        { count: 5000, label: "Books" },
+        { count: 15000, label: "Citations" },
+        { count: 1, label: "Text" },
+      ],
+      services: [
+        { icon: "icon-cloud-download", title: "Quartz", description: "Static Site Generator" },
+        { icon: "icon-laptop", title: "Obsidian", description: "Personal Knowledge Archive" },
+        { icon: "icon-columns", title: "MongoDb", description: "Scalable Media Database" },
+        { icon: "icon-gear", title: "kbMgr", description: "Robust Data Orchestration" },
+      ],
+    },
+    /*
+    pricing: {
+      title: "Plans Built For Every One",
+      description:
+        "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove.",
+      plans: [
+        {
+          name: "Starter",
+          price: "$7/mo",
+          description: "Basic customer support for small business",
+          features: ["10 projects", "20 Pages", "20 Emails", "100 Images"],
+        },
+        {
+          name: "Regular",
+          price: "$19/mo",
+          description: "Basic customer support for small business",
+          features: ["15 projects", "40 Pages", "40 Emails", "200 Images"],
+        },
+        {
+          name: "Plus",
+          price: "$79/mo",
+          description: "Best value",
+          features: ["Unlimited projects", "100 Pages", "100 Emails", "700 Images"],
+        },
+        {
+          name: "Enterprise",
+          price: "$125/mo",
+          description: "For larger enterprises",
+          features: ["Unlimited projects", "Unlimited Pages", "Unlimited Emails", "Unlimited Images"],
+        },
+      ],
+    },
+    */
+     /*
+    team: {
+      title: "Our Staff",
+      description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+      members: [
+        {
+          name: "Jared McCoy",
+          position: "Developer / Designer / Educator",
+          image: "images/user-1.jpg",
+          description: "Building knowledge infrastructure, visualizing the intricacy of language.",
+          social: [
+            { icon: "icon-facebook", link: "#" },
+            { icon: "icon-twitter", link: "#" },
+            { icon: "icon-dribbble", link: "#" },
+            { icon: "icon-github-alt", link: "#" },
+          ],
+        },
+      
+        {
+          name: "Kevin Steve",
+          position: "Co-Founder, Product Designer",
+          image: "images/user-2.jpg",
+          description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+          social: [
+            { icon: "icon-facebook", link: "#" },
+            { icon: "icon-twitter", link: "#" },
+            { icon: "icon-dribbble", link: "#" },
+            { icon: "icon-github-alt", link: "#" },
+          ],
+        },
+        {
+          name: "Ross Standford",
+          position: "Full Stack Developer",
+          image: "images/user-3.jpg",
+          description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.",
+          social: [
+            { icon: "icon-facebook", link: "#" },
+            { icon: "icon-twitter", link: "#" },
+            { icon: "icon-dribbble", link: "#" },
+            { icon: "icon-github-alt", link: "#" },
+          ],
+        },
+      ],
+    },
+    */
+    explore: {
+      title: "Projects",
+      projects: [
+        {
+          image: "images/Receptive_Mindset.png",
+          title: "Sensuous Certainty",
+          caption: "Prototype for a Visual Exploration of Philosophical Complexity",
+        },
+        {
+          image: "images/Human_as_biotech.jpg",
+          title: "Human / Nature / Technics",
+          caption: "Are we really more natural than the machines that define us?",
+        },
+        {
+          image: "images/Scholars-at-Sea-B.webp",
+          title: "Surface / Scholar",
+          caption: "Art thou a scholar? Or art thou adrift?",
+        },
+        {
+          image: "images/Mirrored_Constellations.webp",
+          title: "Text Machines",
+          caption: "Mnemotechnial Infrastructure as Exappropriation ",
+        },
+        {
+          image: "images/Grasping_of_Begreifen.png",
+          title: "Knowledge Infrastructure",
+          caption: "From personal to collective knowledgebase",
+        },
+      ],
+    },
+    gettingStarted: {
+      image: "images/full_image_1.jpg",
+      title: "Getting Started",
+      description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia...",
+      cta: "Get in touch"
+    }, 
+  },
+  footer: {
+    about: {
+      title: "About Us",
+      description: "Far far away, behind the word mountains, far from the countries Vokalia and Consonantia...",
+    },
+    address: {
+      title: "Our Address",
+      "details": [
+        { "icon": "icon-map-marker", "text": "Long Beach, CA" },
+        //{ "icon": "icon-phone", "text": "+ 1235 2355 98" },
+        { "icon": "icon-envelope", "text": "<a href='#'>info@surfacscholar.com</a>" },
+        { "icon": "icon-globe2", "text": "<a href='#'>www.surfacescholar.com</a>" }
+      ]
+    },
+    socialTitle: "Connect with Us", 
+    social: [
+      { icon: "icon-facebook", link: "#" },
+      { icon: "icon-twitter", link: "#" },
+      { icon: "icon-dribbble", link: "#" },
+      { icon: "icon-github-alt", link: "#" },
+    ],
+    contactFormTitle: "Drop us a line",  
+    copyright: "&copy; 2015 Twist Free Template. All Rights Reserved. Designed by FREEHTML5.co",
+  },
+  
+  
+
+};
